3acf5fccbf39785eda25d0f7c07dd9bacc109e7d
Asad Mokarim <asadmokarim@yahoo.com>
2026-04-13 23:38:23 +0530

    update Sidebar: add class sideElement

--- frontend/src/components/admin/Sidebar.jsx
+++ frontend/src/components/admin/Sidebar.jsx
@@ -10,23 +10,41 @@ import LibraryBooksOutlinedIcon from '@mui/icons-material/LibraryBooksOutlined';
 import { NavLink } from "react-router-dom"
 
 
+// #287bff
+
 export default function Sidebar() {
-    const [open, setOpen] = useState(true)
+    const [open, setOpen] = useState(true);
+    const [hovered, setHovered] = useState(null);
 
     return (
 
-        <div className={`bg-white  p-4 transition-all fixed top-0 left-0 h-screen w-64 bg-white shadow ${open ? "w-64" : "w-4"}`}>
+        <div
+            className={`bg-[#287bff] pt-4 transition-all delay-100 overflow-hidden fixed top-0 left-0 h-screen w-64  ${open ? "w-64" : "w-4"}`}
+            style={{
+                borderLeft: "10px solid #287bff"
+            }}
+
+        >
             {/* <button onClick={() => setOpen(!open)} className="mb-4">
                 ☰
             </button> */}
-            <h1 className="text-4xl font-bold font-mono mb-12 text-center p-2">SevaSetu</h1>
+            <h1 className="text-4xl text-white font-semibold font-stretch-expanded font-monto mb-8 text-center p-2">SevaSetu</h1>
 
-            <ul className="space-y-4">
-                <li >
+            <ul
+                className="space-y-12 w-full absolute"
+                onMouseEnter={() => setIsHovering(true)}
+                onMouseLeave={() => setIsHovering(false)}>
+                <li className="mt-5">
                     <NavLink
                         to="/"
+                        onMouseEnter={() => setHovered("/")}
+                        onMouseLeave={() => setHovered(null)}
                         className={({ isActive }) =>
-                            `sideElement flex items-center ${isActive ? "active" : ""
+                            `sideElement flex items-center ${isActive
+                                ? hovered && hovered !== "/"
+                                    ? "active-hover"
+                                    : "active"
+                                : ""
                             }`
                         }
                     >
@@ -37,8 +55,14 @@ export default function Sidebar() {
                 <li >
                     <NavLink
                         to="/volunteers"
+                        onMouseEnter={() => setHovered("/volunteers")}
+                        onMouseLeave={() => setHovered(null)}
                         className={({ isActive }) =>
-                            `sideElement flex items-center ${isActive ? "active" : ""
+                            `sideElement flex items-center ${isActive
+                                ? hovered && hovered !== "/volunteers"
+                                    ? "active-hover"
+                                    : "active"
+                                : ""
                             }`
                         }
                     >
@@ -49,8 +73,14 @@ export default function Sidebar() {
                 <li >
                     <NavLink
                         to="/survey"
+                        onMouseEnter={() => setHovered("/survey")}
+                        onMouseLeave={() => setHovered(null)}
                         className={({ isActive }) =>
-                            `sideElement flex items-center ${isActive ? "active" : ""
+                            `sideElement flex items-center ${isActive
+                                ? hovered && hovered !== "/survey"
+                                    ? "active-hover"
+                                    : "active"
+                                : ""
                             }`
                         }
                     >
@@ -58,11 +88,17 @@ export default function Sidebar() {
                         Survey
                     </NavLink>
                 </li>
-                <li >
+                <li className="mb-5" >
                     <NavLink
                         to="/task"
+                        onMouseEnter={() => setHovered("/task")}
+                        onMouseLeave={() => setHovered(null)}
                         className={({ isActive }) =>
-                            `sideElement flex items-center ${isActive ? "active" : ""
+                            `sideElement flex items-center ${isActive
+                                ? hovered && hovered !== "/task"
+                                    ? "active-hover"
+                                    : "active"
+                                : ""
                             }`
                         }
                     >
